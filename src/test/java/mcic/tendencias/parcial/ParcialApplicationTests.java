20c09a151b87181c75ee581d1a7d7f7d559b76e6
Sebastian Bejarano <slbejaranom@gmail.com>
2024-09-26 19:55:41 -0500

    First commit

---
+++ src/test/java/mcic/tendencias/parcial/ParcialApplicationTests.java
@@ -0,0 +1,13 @@
+package mcic.tendencias.parcial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParcialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
